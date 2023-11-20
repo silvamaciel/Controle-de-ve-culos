64ac46f6694d2a0250c4e6c1c922aa838fb55af7
silvamaciel <macieljunio.si@gmail.com>
2023-11-20 18:25:22 -0300

    atributos  da classe veiculo

--- controle/src/main/java/com/veiculos/Model/Veiculo.java
+++ controle/src/main/java/com/veiculos/Model/Veiculo.java
@@ -1,5 +1,102 @@
 package com.veiculos.Model;
 
 public class Veiculo {
+
+    private Long id;
+    private String placa;
+    private String modelo;
+    private String marca;
+    private String cor;
+    private String ano;
+    private String tipo;
+    private String cnh;
+    private String renavam;
+
+    public Veiculo() {}
+
+    public Veiculo(Long id, String placa, String modelo, String marca, String cor, String ano, String tipo, String cnh, String renavam) {
+        this.id = id;
+        this.placa = placa;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.cor = cor;
+        this.ano = ano;
+        this.tipo = tipo;
+        this.cnh = cnh;
+        this.renavam = renavam;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCnh() {
+        return cnh;
+    }
+
+    public void setCnh(String cnh) {
+        this.cnh = cnh;
+    }
+
+    public String getRenavam() {
+        return renavam;
+    }
+
+    public void setRenavam(String renavam) {
+        this.renavam = renavam;
+    }
+
     
 }
