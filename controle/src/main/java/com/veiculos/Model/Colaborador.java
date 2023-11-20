bc73708d3fbf294fea136c7062bedcf4e81784af
silvamaciel <macieljunio.si@gmail.com>
2023-11-20 18:15:18 -0300

    configuração da class colaborador

--- controle/src/main/java/com/veiculos/Model/Colaborador.java
+++ controle/src/main/java/com/veiculos/Model/Colaborador.java
@@ -2,4 +2,41 @@ package com.veiculos.Model;
 
 public class Colaborador {
     
+    private Long id;
+    private String nome;
+    private String matricula;
+
+    public Colaborador() {}
+
+    public Colaborador(Long id, String nome, String matricula) {
+        this.id = id;
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    
 }
